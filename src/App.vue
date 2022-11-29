949d25514c59d132cd149907640e3e87a474d145
Sergey <voodoograywood@gmail.com>
2022-11-29 10:10:32 +0700

    add modal window to add post

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="app">
-    <post-form
-        @create="createPost"
-    />
+    <h1>Страница с постами</h1>
+    <my-button
+      @click="showDialog"
+      style="margin: 15px 0"
+    >
+      Создать пост
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
     <post-list
         :posts="posts"
+        @remove="removePost"
     />
   </div>
 </template>
@@ -26,12 +36,20 @@ import PostList from "@/components/PostList";
          {id:3, title: 'JavaScript 3', body: 'Описание поста 3'},
          {id:3, title: 'JavaScript 4', body: 'Описание поста 4'},
        ],
+        dialogVisible: false
       }
     },
     methods: {
       createPost(post) {
         this.posts.push(post)
+        this.dialogVisible = false
       },
+      removePost(post) {
+        this.posts = this.posts.filter(p => p.id !== post.id)
+      },
+      showDialog() {
+        this.dialogVisible = true
+      }
     }
   }
 </script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
+  <div v-if="posts.length > 0">
     <h3>Список пользователей</h3>
     <post-item
         v-for="post in posts"
         :post="post"
         key="post.id"
+        @remove="$emit('remove', post)"
     />
   </div>
-
-
+  <h2 v-else style="color: red">
+    Список постов пуст
+  </h2>
 </template>
 
 <script>
