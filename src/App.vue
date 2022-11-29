eb8137e7f8e7534936204ffa5e15b5d27e86fb19
Sergey <voodoograywood@gmail.com>
2022-11-29 16:12:22 +0700

    added post search

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
+    <my-input
+        v-model="searchQuery"
+        placeholder="Поиск..."
+    />
     <div class="app_btn">
       <my-button
           @click="showDialog"
@@ -18,7 +22,7 @@
       />
     </my-dialog>
     <post-list
-        :posts="sortedPosts"
+        :posts="sortedAndSearchedPosts"
         @remove="removePost"
         v-if="!isPostsLoading"
     />
@@ -44,6 +48,7 @@ export default {
       dialogVisible: false,
       isPostsLoading: false,
       selectedSort: '',
+      searchQuery: '',
       sortOptions: [
         {value: 'title', name: 'По названию'},
         {value: 'body', name: 'По содержимому'},
@@ -79,9 +84,13 @@ export default {
   computed: {
     sortedPosts() {
       return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
     }
   },
-  watch: {}
+  watch() {
+  }
 }
 </script>
 
