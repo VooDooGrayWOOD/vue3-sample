a0e86087c7d8a33f1fe762ba0c7956bd12b1aa79
Sergey <voodoograywood@gmail.com>
2022-11-29 15:47:21 +0700

    added sorting of posts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <my-button
-      @click="showDialog"
-      style="margin: 15px 0"
-    >
-      Создать пост
-    </my-button>
+    <div class="app_btn">
+      <my-button
+          @click="showDialog"
+      >
+        Создать пост
+      </my-button>
+      <my-select
+          v-model="selectedSort"
+          :options="sortOptions"
+      />
+    </div>
     <my-dialog v-model:show="dialogVisible">
       <post-form
           @create="createPost"
       />
     </my-dialog>
     <post-list
-        :posts="posts"
+        :posts="sortedPosts"
         @remove="removePost"
         v-if="!isPostsLoading"
     />
@@ -25,60 +30,75 @@
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
 import axios from "axios";
+import MySelect from "@/components/UI/MySelect";
 
-  export default {
-    components: {
-      PostList,
-      PostForm
-    },
-    data() {
-      return {
-       posts: [],
-        dialogVisible: false,
-        isPostsLoading: false
-      }
-    },
-    methods: {
-      createPost(post) {
-        this.posts.push(post)
-        this.dialogVisible = false
-      },
-      removePost(post) {
-        this.posts = this.posts.filter(p => p.id !== post.id)
-      },
-      showDialog() {
-        this.dialogVisible = true
-      },
-      async fetchPosts() {
-        try {
-          this.isPostsLoading = true
-            const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
-            this.posts = response.data
-        }catch (e) {
-          alert('Ошибка')
-        } finally {
-          this.isPostsLoading  = false
-        }
-      }
-    },
-    mounted() {
-      this.fetchPosts()
+export default {
+  components: {
+    PostList,
+    PostForm,
+    MySelect
+  },
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По содержимому'},
+      ]
     }
-  }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+      this.dialogVisible = false
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true
+    },
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        this.posts = response.data
+      } catch (e) {
+        alert('Ошибка')
+      } finally {
+        this.isPostsLoading = false
+      }
+    }
+  },
+  mounted() {
+    this.fetchPosts()
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    }
+  },
+  watch: {}
+}
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-  .app {
-    padding: 20px;
-  }
+.app {
+  padding: 20px;
+}
 
-
-
-
+.app_btn {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
